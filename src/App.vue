3f60d201a2d15484dfce681e82d3de4bd0f7db9c
Declan Blanchard <declanblanc@gmail.com>
2024-11-02 15:19:37 -0400

    Navigation bar with sample links

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,17 @@
-<script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
+  <Testing />
   <div class="text-blue-500">Hello World</div>
   <RouterView />
 </template>
+
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import Testing from './components/Testing.vue';
+
+
+</script>
+
+
 
 <style scoped>
 
---
+++ src/components/Testing.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <div id="main-nav">
+        <a>Home</a>
+        <a>About</a>
+        <a>Contact</a>
+        <a>Blog</a>
+    </div>
+</template>
+
+<style scoped>
+    #main-nav {
+        display: flex;
+        justify-content: space-around;
+        background-color: blue;
+    }
+</style>
