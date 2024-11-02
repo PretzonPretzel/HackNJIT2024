a79beb92a88dbc66d20cc56383e35167a27403a1
Nick Triano <nicholastriano1999@gmail.com>
2024-11-02 15:21:41 -0400

    added part customization options component

---
+++ src/components/ItemOptions.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <div>Part To Customize</div>
+      <div>Component:</div>
+        <div>Option 1</div>
+      <div>Colors:</div>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+interface ColorOption {
+  name: string
+  hex: string
+}
+
+interface ComponentOption {
+  name: string
+  id: string
+  colorOptions: ColorOption[]
+}
+
+interface PartCustomizationOption {
+  partName: string
+  components: ComponentOption[]
+}
+
+const itemOptions: PartCustomizationOption[] = [
+  {
+    partName: "Glass",
+    components: [
+      {
+        name: "Component 1",
+        id: "",
+        colorOptions: [
+          {
+            name: "red",
+            hex: "#FFFF"
+          }
+        ]
+      }
+    ]
+  }
+]
+
+</script>
+
+<style scoped>
+
+</style>
