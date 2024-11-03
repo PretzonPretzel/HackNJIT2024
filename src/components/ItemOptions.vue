282330631cb793565fffcb362467c889eeca8347
Nick Triano <nicholastriano1999@gmail.com>
2024-11-03 01:18:33 -0400

    start of options menu

--- src/components/ItemOptions.vue
+++ src/components/ItemOptions.vue
@@ -1,124 +1,37 @@
 <template>
-  <div id="page-container">
-
-    <div id="img-container">
-      <img src="../assets/logo.svg" id="prod-img"></img>
-    </div>
-    
-    <div id="options-container">
-      <h1 class="prod-title">The Vue Logo</h1>
-      
-      <div class="color-selector">
-        <p>Logo Colors</p>
-        <div class="color-options">
-          <button>Default</button>
-          <button>Red</button>
-          <button>Green</button>
-        </div>
+  <div class="options-container">
+    <div>Options Menu</div>
+    <div v-for="option in options">
+      <div class="part-title">Part: {{ option.partName }}</div>
+      <div v-for="component in option.components">
+        <div class="component-name">{{component.name}}</div>
       </div>
-
-      <div class="comp-options">Component:</div>
-        <div>Option 1</div>
-      <div>Colors:</div>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
+import type { PartCustomizationOption } from '@/types/types';
 
-
-
-interface ColorOption {
-  name: string
-  hex: string
-}
-
-interface ComponentOption {
-  name: string
-  id: string
-  colorOptions: ColorOption[]
-}
-
-interface PartCustomizationOption {
-  partName: string
-  components: ComponentOption[]
-}
-
-const itemOptions: PartCustomizationOption[] = [
-  {
-    partName: "Glass",
-    components: [
-      {
-        name: "Component 1",
-        id: "",
-        colorOptions: [
-          {
-            name: "red",
-            hex: "#FFFF"
-          }
-        ]
-      }
-    ]
-  }
-]
-
+const props = defineProps<{
+  options: PartCustomizationOption[]
+}>()
 </script>
 
 <style scoped>
-
-div {
-  border: 2px solid blue;
+.part-title {
+  margin-left: 2rem;
 }
 
-#page-container {
-  background-color: #281511;
-  height: 100vh;
-  color: white;
-
+.component-name {
+  margin-left: 4rem;
 }
-
-.color-options button {
-  background-color: #963501;
-  width: 150px;
-  height: 75px;
-  margin: 10px;
-  border: 3px solid black;
-}
-.color-selector {
+.options-container {
+  border: 2px solid black;
+  width: 200px;
   display: flex;
   flex-direction: column;
-  align-items: left;
-}
-.color-options {
-  display: flex;
-  justify-content: space-around;
+  justify-content: center;
   align-items: center;
-  padding: 20px;
-}
-
-.prod-title {
-  font-size: 50px;
-  font-weight: 900;
-}
-
-#page-container {
-  display: flex;
-}
-
-#options-container {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-}
-
-#img-container {
-  height: 500px;
-  width: 500px;
-  /* border-image: url('https://cdn.pixabay.com/photo/2021/08/08/19/35/gears-6531778_960_720.png') 30 round; */
-  border: 16px solid black;
-}
-
-#prod-img {
-  padding: 30px;
 }
 </style>
--- src/views/ItemView.vue
+++ src/views/ItemView.vue
@@ -1,16 +1,21 @@
 <template>
   <div class="item-view-container">
-    <ItemRenderView 
-      v-if="currentItem"
-      :file-name="currentItem?.fileName"
-      :current-item="currentItem"
-    />
+    <div v-if="currentItem" class="item-view">
+      <ItemRenderView 
+        :file-name="currentItem?.fileName"
+        :current-item="currentItem"
+      />
+      <ItemOptions :options="customizationOptions"/>
+    </div>
     <div v-else>Item Not Found</div>
+    
+    
   </div>
 </template>
 
 <script setup lang="ts">
 import ItemRenderView from '@/components/ItemRenderView.vue';
+import ItemOptions from '@/components/ItemOptions.vue';
 import { storeItems } from '@/data/storeItems';
 import { computed } from 'vue';
 
@@ -19,6 +24,7 @@ const props = defineProps<{
 }>()
 
 const currentItem = computed(() => storeItems.find(item => item.id === props.id))
+const customizationOptions = computed(() => currentItem.value?.modelData.customizationOptions ?? [])
 
 </script>
 
@@ -28,5 +34,13 @@ const currentItem = computed(() => storeItems.find(item => item.id === props.id)
   display: flex;
   justify-content: center;
   align-items: center;
+}
+
+.item-view {
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: start;
+  gap: 1rem;
 }
 </style>
